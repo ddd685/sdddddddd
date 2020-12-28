3799794ca2622936a7290747bd862ab09f0b9c31
ddd685 <75838754+ddd685@users.noreply.github.com>
2020-12-28 16:53:16 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,29 @@
+canvas= document.getElementById("me_canvas");
+ctx= canvas.getContext("2d");
+color= "blue";
+
+ctx.beginPath();
+ctx.strokeStyle= color;
+ctx.lineWidth= 2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown",M_D);
+function M_D(e){
+    color=document.getElementById("color").value;
+    console.log(color);
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    console.log("x="+mouse_x+",y="+mouse_y);
+    circle(mouse_x,mouse_y);
+}
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle= color;
+    ctx.lineWidth= 2;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+    ctx.stroke(); 
+}
+function Clear(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
